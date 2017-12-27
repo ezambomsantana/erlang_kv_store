f0238bf73f5b93cd52b1c385455223f183a17313
ezambomsantana <ezambomsantana@gmail.com>
2017-12-26 22:35:53 -0200

    new functions

--- src/store.erl
+++ src/store.erl
@@ -6,13 +6,7 @@
 
 -behaviour(gen_server).
 
-%% Application callbacks
--export([start/0, stop/1, start_link/1, init/1, store/3, handle_call/3, fetch/2, delete/2, all_keys/1]).
-
-%% ===================================================================
-%% API (Global)
-%% ===================================================================
-%% @doc Starts the application	
+-export([start/0, stop/1, start_link/1, init/1, store/3, handle_call/3, fetch/2, delete/2, all_keys/1, lookup/2, update/3]).
 
 -spec start() -> {ok, _} | {error, term()}.
 start() ->
@@ -43,6 +37,12 @@ delete(Pid, Key) -> gen_server:call(Pid, {delete, Key}).
 -spec all_keys(store()) -> [{key(), value()}].
 all_keys(Pid) -> gen_server:call(Pid, {all_keys}).
 
+-spec lookup(store(), value()) -> [key()].
+lookup(Pid, Value) -> gen_server:call(Pid, {lookup, Value}).
+
+-spec update(store(), key(), value()) -> {ok, value()} | {error, not_found}.
+update(Pid, Key, Value) -> gen_server:call(Pid, {update, Key, Value}).
+
 handle_call({store, Key, Value}, _From, State) ->
    put (Key , Value),
    {reply, ok, State };
@@ -60,9 +60,21 @@ handle_call({fetch, Key}, _From, State) ->
 
 handle_call({all_keys}, _From, State) ->
    List = get(),
-   {reply, List, State }.
+   {reply, List, State };
 
-%get_keys(Value)
+handle_call({lookup, Value}, _From, State) ->
+   List = get_keys(Value),
+   {reply, List, State };
+
+handle_call({update, Key, Value}, _From, State) ->
+   case get(Key) of
+      undefined -> {reply, {error, not_found}, State };
+      OldValue ->
+         put(Key, Value),
+         {reply, {ok, OldValue}, State }
+   end.
+
+
 
 code_change(_OldVsn, State, _Extra) ->
 {ok, State}.
